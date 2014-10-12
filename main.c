b6164e1cbb67304dd6e1c2f6cfdbaba1f9745c1c
alextpedro <alexandra.dragon@gmail.com>
2014-10-12 04:00:37 +0100

    Adicionados ficheiros base

---
+++ main.c
@@ -0,0 +1,37 @@
+/**
+* @file main.c
+* @brief Description
+* @date 01-01-2006
+* @author eiYxxxxx@student.estg.ipleiria.pt
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
+
+#include "debug.h"
+#include "memory.h"
+
+int main(int argc, char *argv[])
+{
+	/* Disable warnings */
+	(void)argc; (void)argv;
+
+	/* Main code */
+
+
+	return 0;
+}
+
