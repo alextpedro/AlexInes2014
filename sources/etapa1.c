8bc0535fe1e73b0bafd11c5dbcb2d6d2534bfc5a
InesFaria <inescristofaria22@hotmail.com>
2014-12-29 22:19:21 +0000

    ficheiro original, sem testes

--- sources/etapa1.c
+++ sources/etapa1.c
@@ -267,8 +267,7 @@ int write_to_file (char** words, char *filename, FILE* compressed, unsigned int 
  * @return Returns 0 if succesful.
  */
 int folderDecompress (const char *dirname) {
-	printf("cheguei\n");
-	/*int function = 2;
+	int function = 2;
 	LISTA_GENERICA_T* listOfDir = lista_criar(NULL);
 	lista_inserir_inicio(listOfDir, strdup(dirname));
 
@@ -319,5 +318,5 @@ int folderDecompress (const char *dirname) {
 	}
 
 	lista_destruir(&listOfDir);
-	return 0;*/
+	return 0;
 }
